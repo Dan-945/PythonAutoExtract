1eab6883e3599ce330cc2bfef560fef3a72b8f16
Pyfrig <dan.helgeland@gmail.com>
2019-11-09 13:34:09 +0100

    Nye funksjoner laget

---
+++ autoExtract.py
@@ -0,0 +1,14 @@
+import os, logging
+
+#TODO sett opp logging
+
+#TODO søk igjennom download folder
+
+
+
+#TODO skill ut kilder med og uten .rar lag liste med rene .mkv downloads og kopier disse til plex mappe.
+
+
+#TODO extract nye filmer.
+
+#TODO legg liste for ferdig extract filmer i liste for exclude i neste søk?
---
+++ moveFiles.py
@@ -0,0 +1,10 @@
+import os, logging
+
+#TODO logging, set opp log fil system.
+
+
+#TODO søk igjennom mapper for nye .mkv etc filer.
+
+#TODO flytt filer til ny folder for filmer brukt av plex
+
+
