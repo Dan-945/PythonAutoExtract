e7bcd4256fd415deb6a5125ddaeb54c786292472
pyfrig <dan.helgeland@gmail.com>
2019-11-11 19:22:43 +0100

    Made sort function - some fix to debug / info

--- autoExtract.py
+++ autoExtract.py
@@ -1,29 +1,37 @@
-import os, logging, rarfile
+import os, logging, rarfile, moveFiles
 
 logging.basicConfig(level=logging.DEBUG, format='%(asctime)s, - %(levelname)s %(message)s')
-
+#logging.disable(logging.DEBUG)
 # TODO log to txt file when done.
-searchPath = r'/home/thebox/SeagateDisk/MediaFolder/completed'
+searchPath = r'/home/thebox/SeagateDisk/MediaFolder/testfolder'
 
 filesToExtract = []
-os.chdir(searchPath)
+#os.chdir(searchPath) #Not necessary as all paths used are absolute?
 
 # Search through folders to find all rar files to be extracted.
 def folderContainsRar(folder):
     dir_listing = os.listdir(folder)
     for file in dir_listing:
         if file.endswith('.rar'):
-            logging.info('rar file found')
+            logging.debug('rar file found')
             if os.path.exists(os.path.join(folder, 'unrared.txt')):
-                logging.info('file already extracted, will be skipped') #TODO legg til kort beskrivelse av sti.
+                logging.debug('%s file already extracted, will be skipped' % (file))
             else:
                 filesToExtract.append(os.path.join(searchPath, folder, file))
-                logging.info('added for extracting') #TODO legg til kort beskrivelse av sti.
+                logging.info('%s added for extracting' % (file))
+        if file.endswith('.mkv'):
+            logging.debug('mkv file found')
+            if os.path.exists(os.path.join(folder, 'copied.txt')):
+                  logging.debug('file already copied, will be skipped')   
+            else:
+                #os.popen('cp'+(os.path.join(searchPath, folder, file))+ moveFiles.fileSort(file)) #TODO sorting function needed
+                logging.debug('file will be copied to %s' % (moveFiles.fileSort(file)))
+                logging.info('%s file to be copied' % (file))
     return
 
 # walk through all folders to check content.
 def searchFolders(searchPath):
-    logging.debug('Searching through folder')
+    logging.info('Searching through folder')
     for folderName, subFolders, fileNames in os.walk(searchPath):
         folderContainsRar(folderName)
     logging.info('Search complete')
@@ -32,14 +40,15 @@ def searchFolders(searchPath):
 def unrar():
     for i in range(len(filesToExtract)):
         x = rarfile.RarFile(filesToExtract[i])
-        x.extractall()                          #TODO make handling for placement of extracted files.
+        #x.extractall(moveFiles.fileSort(filesToExtract[i]))                          #TODO sorting function needed
+        logging.debug('file will be extracted to %s' % (moveFiles.fileSort(filesToExtract[i])))
         os.mknod(os.path.join(os.path.dirname(filesToExtract[i]),'unrared.txt'))
         # x.close() #TODO do i need this?...
     return
 
 searchFolders(searchPath)
-logging.debug('Files to be extracted: %s ' % (filesToExtract))
+logging.info('Files to be extracted: %s ' % (filesToExtract))
 
-#unrar()
+unrar()
 
-# TODO skill ut kilder med og uten .rar lag liste med rene .mkv downloads og kopier disse til plex mappe.
+#TODO sorting function needed
--- moveFiles.py
+++ moveFiles.py
@@ -1,9 +1,22 @@
 import os, logging
 
-#TODO logging, set opp log fil system.
+logging.basicConfig(level=logging.DEBUG, format='%(asctime)s, - %(levelname)s %(message)s')
 
+#real destination
+#destinationPath = r'/home/thebox/SeagateDisk/MediaFolder/PLEX MEDIA FOLDER'
+#test destination
+destinationPath = r'/home/thebox/SeagateDisk/MediaFolder/testfolder'
 
-#TODO søk igjennom mapper for nye .mkv etc filer.
+#sort file into folder based on beginning letter of filename.
+#only filename expected as input
+x = 'brooklyn.nine-nine.s06e01.multi.720p.web.h264-cielos.mkv'
+def fileSort(file):
+    destinationFolder = ''
+    destinationFolder = file[0].upper()
+    #print(destinationPath +'/'+ destinationFolder)
+    return destinationPath +'/'+ destinationFolder
+    
+#fileSort(x)
 
 #TODO flytt filer til ny folder for filmer brukt av plex
 
